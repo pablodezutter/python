46f18f0fdace967bd055b95f4d3a758bb8c27e23
Pablo Dezutter <pablodezutter@gmail.com>
2022-06-02 22:58:28 +0200

    q&a

---
+++ exercises.py
@@ -0,0 +1,6 @@
+# string concat
+link = ""#some link
+
+print("Go to" + link)
+print("Go to {}".format (link))
+print(f"Go to {link}")
---
+++ meet.py
@@ -0,0 +1,16 @@
+name = input ('What is your name? ')
+
+if(name) != str:
+    print('Is that your real name? Please try typing it again')
+# loop nodig om terug te keren    
+else: 
+    print(' Hello ' + name)
+
+color = input('What is your favorite color? ')
+print( ' Wow! ' + color + ' is a nice color. ')
+
+language = input('What is your language? ')
+if (language) != 'English':
+        print('I still struggle with ' + language)
+else:
+        print('Nice! Our conversations will be easy! ')
