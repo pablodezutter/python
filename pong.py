ddb4dd5208dd17c9792445d7ab4ba844ee344d19
Pablo Dezutter <pablodezutter@gmail.com>
2022-07-14 21:35:55 +0200

    paddles

--- pong.py
+++ pong.py
@@ -10,17 +10,41 @@ FPS = 60
 white = (255,255,255)
 black = (0,0,0)
 
-def draw():
-    win.fill(white)
-pygame.display.update()
+PADDLE_WIDTH, PADDLE_HEIGHT= 20,100
+
+
+class Paddle:
+    color = white
+
+    def __init__(self,x,y,width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def draw (self, win):
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
+
+def draw(win, paddles):
+    win.fill(black)
+
+    for paddle in paddles:
+        paddle.draw(win)
+
+    pygame.display.update()
 
 def main():
     run = True
     clock = pygame.time.Clock()
 
+    left_paddle = Paddle(10, height//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT )
+    right_paddle = Paddle(width - 10 - PADDLE_WIDTH, height //2 - PADDLE_HEIGHT //2,  PADDLE_WIDTH, PADDLE_HEIGHT)
 
     while run:
         clock.tick(FPS) ##regulate speed
+        draw(win, [left_paddle, right_paddle])
+
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
