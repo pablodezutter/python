df3006705f3425b24e652995c7cc3d6201ec2c16
Pablo Dezutter <pablodezutter@gmail.com>
2022-07-14 20:31:06 +0200

    14072022 problem with clock

--- pong.py
+++ pong.py
@@ -5,14 +5,21 @@ width, height = 700, 500
 win = pygame.display.set_mode((width,height))
 pygame.display.set_caption("PONG")
 
+FPS = 70
+
 def main():
     run = True
+    clock = pygame.time.Clock()
+
 
     while run:
+        clock.tick(FPS) ##regulate speed
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+
     pygame.quit()
 
 if __name__ == '__main__':
