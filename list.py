330fb89d64ed5db34909131cda91aaf57b7efa84
Pablo Dezutter <pablodezutter@gmail.com>
2022-10-05 20:55:39 +0200

    list

---
+++ list.py
@@ -0,0 +1,12 @@
+seats = [
+['a', 'b', 'c'],
+['d', 'e', 'f'],
+['g', 'h', 'i'],
+['j', 'k', 'l']
+]
+
+#your code goes here
+seatRow = int(input())
+seatColumn = int(input())
+
+print (seats[seatRow][seatColumn])
