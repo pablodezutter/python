e5c3dac1ae5fb1bae87fd0499c7d606b7d111d1a
Pablo Dezutter <pablodezutter@gmail.com>
2022-10-17 21:32:34 +0200

    going down

--- range.py
+++ range.py
@@ -1,7 +1,12 @@
 n = int(input())
 
-restRooms = range(5,n,5)
+"""restRooms = range(5,n,5)
 for n in restRooms:
  print(n)
  if n == 15: 
-    print('big toilet here') 
+    print('big toilet here') """
+
+
+restRooms = range(n,0,-2)
+for n in restRooms:
+ print(n)
