e1e1154971c3d4e2bee7f4ec62c042053145edde
Pablo Dezutter <pablodezutter@gmail.com>
2022-11-09 20:47:39 +0100

    lists

---
+++ back.py
@@ -0,0 +1,12 @@
+##x = input()
+##print(x[::-1])
+
+#list = [1,1,2,3,5,8,13]
+#print(list[list[4]]) 
+# eerst binnnenste lijst print =  5, daarna buitenste lijst die deze uitkomtst gebruikt als parameter
+
+for i in range(10):
+  if not i % 2 == 0:
+    print(i+1)
+
+##Since it is a range and the maximum value is 10, then it will take values from 2 to 8 Which are even numbers between 2 and 1
